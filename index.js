45fcb95f55c3ef634e2ff85c4ad54a433ea56e5e
eundii <eunjee1504@naver.com>
2023-04-16 00:10:09 +0900

    mongoose로 Post 모델 생성

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const path = require("path");
-// 몽구스 디비 선언
 const mongoose = require("mongoose");
 
 const app = express();
 const port = 5000;
+
+// Post 모델 사용을 위해 불러오기
+const { Post } = require("./Model/Post.js");
 
 app.use(express.static(path.join(__dirname, "../client/build")));
 // client에서 보내는 body에 대한 명령어를 추적할 수 있게 설정
@@ -16,7 +18,7 @@ app.listen(port, () => {
   // 서버가 열리면 몽구스 디비를 연결
   mongoose
     .connect(
-      "mongodb+srv://eundii:1q2w3e4r@eundii.cqizn6z.mongodb.net/?retryWrites=true&w=majority"
+      "mongodb+srv://eundii:1q2w3e4r@eundii.cqizn6z.mongodb.net/Community?retryWrites=true&w=majority"
     )
     .then(() => {
       console.log(
@@ -38,9 +40,9 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
-// 서버에서도 구현
 app.post("/api/test", (req, res) => {
-  console.log(req.body);
-  // 요청 성공시 클라이언트에게 json타입으로 값을 보내줄 수 있음
-  res.status(200).json({ success: true });
+  const CoummunityPost = new Post({ title: "test", content: "테스트입니다!" });
+  CoummunityPost.save().then(() => {
+    res.status(200).json({ success: true });
+  });
 });
