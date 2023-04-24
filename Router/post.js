70d9a6516983e1f1ac246adf7e4c31f2bc9051d9
eundii <eunjee1504@naver.com>
2023-04-24 10:36:05 +0900

    게시글 정보와 회원 정보 연동

--- Router/post.js
+++ Router/post.js
@@ -4,10 +4,15 @@ const router = express.Router();
 
 const { Post } = require("../Model/Post.js");
 const { Counter } = require("../Model/Counter.js");
+const { User } = require("../Model/User.js");
 
 const setUpload = require("../util/upload.js");
 router.post("/submit", (req, res) => {
-  let temp = req.body;
+  let temp = {
+    title: req.body.title,
+    content: req.body.content,
+    image: req.body.image,
+  };
   // Counter 모델에서 postNum을 찾기
   // find에는 조건을 걸 수 있다. 조건은 name이 counter 인 것(MongoDB에서 insertDocument로 넣음)
   Counter.findOne({ name: "counter" })
@@ -15,22 +20,31 @@ router.post("/submit", (req, res) => {
     .then((counter) => {
       // 받아온 데이터에 counter의 postNum을 넣어준다.
       temp.postNum = counter.postNum;
-      // 그 다음 DB에 저장하기
-      const CoummunityPost = new Post(temp);
-      CoummunityPost.save()
-        .then(() => {
-          // postNum을 1 증가시켜야함
-          // MongoDB에서 하나의 document를 업데이트 시키려면 updateOne 명령어 사용(쿼리 2개 받음)
-          // 첫번째 쿼리에는 어떤document를 업데이트 할지, 두번째는 어떻게 업데이트 할건지
-          // 쿼리문에서 증가시키는 기능은 $inc 사용(postNum을 1만큼 증가)
-          Counter.updateOne({ name: "counter" }, { $inc: { postNum: 1 } }).then(
-            () => {
-              res.status(200).json({ success: true });
-            }
-          );
-        })
-        .catch((error) => {
-          res.status(400).json({ success: false });
+
+      // User 모델에서 똑같은 uid를 가진 객체를 찾아서 author에 _id를 가진 모든 정보를 넣어준다.
+      User.findOne({ uid: req.body.uid })
+        .exec()
+        .then((userInfo) => {
+          temp.author = userInfo._id;
+
+          // 그 다음 DB에 저장하기
+          const CoummunityPost = new Post(temp);
+          CoummunityPost.save()
+            .then(() => {
+              // postNum을 1 증가시켜야함
+              // MongoDB에서 하나의 document를 업데이트 시키려면 updateOne 명령어 사용(쿼리 2개 받음)
+              // 첫번째 쿼리에는 어떤document를 업데이트 할지, 두번째는 어떻게 업데이트 할건지
+              // 쿼리문에서 증가시키는 기능은 $inc 사용(postNum을 1만큼 증가)
+              Counter.updateOne(
+                { name: "counter" },
+                { $inc: { postNum: 1 } }
+              ).then(() => {
+                res.status(200).json({ success: true });
+              });
+            })
+            .catch((error) => {
+              res.status(400).json({ success: false });
+            });
         });
     });
 });
@@ -39,6 +53,7 @@ router.post("/list", (req, res) => {
   // MongoDB에서 document를 찾는 명령어 : find
   // find 명령어가 끝나면(exec) 찾은 명령어(doc)
   Post.find()
+    .populate("author")
     .exec()
     .then((doc) => {
       res.status(200).json({ success: true, postList: doc });
@@ -50,6 +65,7 @@ router.post("/list", (req, res) => {
 
 router.post("/detail", (req, res) => {
   Post.findOne({ postNum: Number(req.body.postNum) })
+    .populate("author")
     .exec()
     .then((doc) => {
       res.status(200).json({ success: true, post: doc });
